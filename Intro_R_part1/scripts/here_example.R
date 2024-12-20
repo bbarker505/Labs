1b0023930052184b107c50b967bfc53abb5ce771
bbarker505 <bbarker505@gmail.com>
2024-12-20 13:00:51 -0800

    udpates

--- Intro_R_part1/scripts/here_example.R
+++ Intro_R_part1/scripts/here_example.R
@@ -7,3 +7,11 @@ here("Intro_R_part1", "files", "file.txt")
 
 # Load the file
 readLines(here("Intro_R_part1", "files", "file.txt"))
+
+# This is the faster and easier way to access this file than:
+readLines("/home/barkebri/Labs/Intro_R_part1/files/file.txt")
+
+# Also, you'd have to change the absolute path every time you move files
+# For example, if you copy labs from this course to your own computer and
+# don't use relative paths, you'd have to change every single line of code
+# where you import files. 
